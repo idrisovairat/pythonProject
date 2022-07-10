e77465cbac9964dd373d82e2110579446a667682
idrisovari <idrisovairat@gmail.com>
2022-07-10 11:05:05 +0300

    третья домашка. вроде как становится проще постепенно. но пока айтон удается сложно.

---
+++ HW1/HW3/homework1-2.py
@@ -0,0 +1,8 @@
+name = input('введите имя:')
+surname = input('введите фамилия:')
+city = input('город:')
+birthday = input('введите год рождения:')
+email = input('введите электронную почту:')
+phone = input('введите состовый телефон:')
+
+print(name, surname, city, birthday, email, phone)
